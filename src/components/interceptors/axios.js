44ff4b5555f3c479920938d59c3c3404ed5d1f9a
Weldegebrial Belete <weldegebrial.gesesse@gmail.com>
2025-01-10 01:11:59 +0300

    gat price edit

--- src/components/interceptors/axios.js
+++ src/components/interceptors/axios.js
@@ -11,7 +11,7 @@ axios.interceptors.response.use(
       const refresh_token = Cookies.get('refresh_token');
 
       if (refresh_token) {
-        const response= await axios.post('v4/student/refresh-token', { refresh_token });
+        const response = await axios.post('v4/student/refresh-token', { refresh_token });
 
         if (response.status === 200) {
           axios.defaults.headers.common['Authorization'] = `Bearer ${response.data}`;
